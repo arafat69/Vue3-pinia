05e43a4acc4afb442498138af93e92d74bc528fa
arafat69 <arafataliar69.com>
2023-05-27 22:07:54 +0600

    pinia tailwind install and config

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="bg-slate-500">
+    <Header/>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue';
+  export default {
+    components:{Header},
+  }
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <header class="bg-gray-300 py-5">
+    <div class="flex justify-center items-center gap-4">
+      <img src="/logo.svg" alt="" class="w-14 -rotate-12">
+      <div class="text-4xl rotate-2">Pinia Task</div>
+    </div>
+      <form action="" class="flex justify-center gap-1 mt-3">
+        <input type="text" placeholder="..." class="w-72 outline-none h-9 focus:ring rounded-sm p-2">
+        <button class="text-white bg-yellow-600 rounded-sm h-9 w-16">Add</button>
+      </form>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
