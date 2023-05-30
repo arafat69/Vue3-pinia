68befe99b74e15c249f65eb8f1d1132864480a72
arafat69 <arafataliar69.com>
2023-05-30 23:45:44 +0600

    improve: task favorite color change and delete color change

--- src/components/TaskDetails.vue
+++ src/components/TaskDetails.vue
@@ -2,8 +2,13 @@
     <div class="bg-white border border-stone-200 rounded p-3 flex justify-between items-center gap-2">
         {{ task.title }}
         <div class="gap-2 flex">
-            <span @click="taskStore.deleteTask(task.id)"><i class="bi bi-trash"></i></span>
-            <span @click="taskStore.toggleFav(task.id)"><i class="bi bi-heart"></i></span>
+            <span @click="taskStore.deleteTask(task.id)" class="cursor-pointer hover:text-red-800">
+                <i class="bi bi-trash"></i>
+            </span>
+            <div @click="taskStore.toggleFav(task.id)" class="cursor-pointer hover:scale-125 transition-all">
+                <span v-if="task.isFav" class="text-red-500"><i class="bi bi-heart-fill"></i></span>
+                <span v-else><i class="bi bi-heart"></i></span>
+            </div>
         </div>
     </div>
 </template>
